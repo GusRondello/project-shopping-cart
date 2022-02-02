2c99fa68acfc8597ac1b9cf4008607e67b20f715
Gustavo Rondello <gustavorondello99@outlook.com>
2022-02-02 16:38:34 -0300

    Feito testes do requisito 8 e feito a função do fetchProducts

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const { fetchProducts } = require('./helpers/fetchProducts');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -39,5 +41,6 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
-
-window.onload = () => { };
+window.onload = () => { 
+  fetchProducts();
+};
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,22 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('fetchProducts é uma `function`', async () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  it('Se ao chamar a função fetchProducts("computador"), a função fetch é chamada', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Se ao chamar a função fetchProducts("computador"), a função fetch utiliza o endpoit certo', async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+  it('Se o retorno de fetchProducts("computador") é uam estrutura de dados igual a computadorSearch', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  });
+  it('Se ao ser chamada sem argumento retorna um erro com a mensagem esperada', async () => {
+    expect(await fetchProducts()).toEqual(new Error('You must provide an url'));
+  });
 });
