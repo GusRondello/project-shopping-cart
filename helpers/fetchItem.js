0954132b7a20f901ad4f307c2ae1ae8fac3551c7
Gustavo Rondello <gustavorondello99@outlook.com>
2022-02-03 14:59:20 -0300

    Requisito 9 feito, fetchItem() completada

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,12 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (idSelecionado) => {
+  const selectedUrl = `https://api.mercadolibre.com/items/${idSelecionado}`;
+  try {
+    const response = await fetch(selectedUrl);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -7,12 +7,18 @@ describe('2 - Teste a função fecthItem', () => {
     expect(typeof fetchItem).toBe('function');
   });
   it('Se ao chamar a função fecthItem("MLB1615760527"), a função fetch é chamada', async () => {
-    await fecthItem('MLB1615760527');
+    await fetchItem('MLB1615760527');
     expect(fetch).toBeCalled();
   });
-  it('Se ao chamar a função fecthItem("MLB1615760527"), a função fetch utiliza o endpoit certo', async () => {
-    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
-    await fetchProducts('computador');
+  it('Se ao chamar a função fetchItem("MLB1615760527"), a função fetch utiliza o endpoit certo', async () => {
+    const url = 'https://api.mercadolibre.com/items/MLB1615760527'
+    await fetchItem('MLB1615760527');
     expect(fetch).toHaveBeenCalledWith(url);
+  });
+  it('Se o retorno de fetchItem("MLB1615760527") é uma estrutuda de dados igual a "item"', async () =>{
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
+  it('Se ao ser chamada sem argumento retorna um erro com a mensagem esperada', async () => {
+    expect(await fetchItem()).toEqual(new Error('You must provide an url'));
   });
 });
