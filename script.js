0f8634e86152cb2126702d067ee5cc65b358d83e
Gustavo Rondello <gustavorondello99@outlook.com>
2022-02-04 19:15:45 -0300

    Requisito 5 completo 100% do projeto feito #VQV :rocket:

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const listaItens = document.querySelector('.items');
 const cartItems = document.querySelector('.cart__items');
 const removeBtn = document.querySelector('.empty-cart');
 const header = document.querySelector('.header');
-/* const cartSection = document.querySelector('.cart');
-const subtotal = document.createElement('p'); */
+const totalPrice = document.querySelector('.total-price');
+let cartValues = 0;
+
+function addCartPrice() {
+  totalPrice.innerHTML = cartValues;
+}// tive ajuda na mentoria do dia 3 do Raphael Martins para realizar corretamente a soma e subtração do valor e consertar o código após isso.
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -40,6 +44,10 @@ function cartItemClickListener(event) {
     event.target.remove(); 
     saveCartItems(cartItems.innerHTML);
   }
+  const clickedValue = event.target.innerHTML.split('$')[1];
+  cartValues -= parseFloat(clickedValue);
+  addCartPrice();
+  console.log(clickedValue);
 } // Essa função remove o item clicado do cart (entendi o paramentro do if correto com ajuda do Lucas Leal na mentoria do 2 dia)
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
@@ -47,8 +55,6 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
-/*   cartSection.appendChild(subtotal);
-  subtotal.innerText = `Subtotal: R$ ${salePrice}`; */
   return li;
 }
 
@@ -56,31 +62,30 @@ function createLoadingElement() {
   const span = document.createElement('span');
   span.className = 'loading';
   span.innerText = 'Carregando...';
-  return span;
+  header.appendChild(span);
 }
 
 function removeLoadingElement() {
   const getLoading = document.querySelector('.loading');
   getLoading.remove();
 }
-function loadInteraction() {
-  header.appendChild(createLoadingElement());
-  setTimeout(() => {
-    removeLoadingElement();
-  }, 1000);
-}
 
 const renderProducts = async () => {
   const computers = await fetchProducts('computador');
+  removeLoadingElement();
   const { results } = computers;
   results.forEach((element) => listaItens.appendChild(createProductItemElement(element)));
 }; // Essa função retorna meus itens na tela (Tive ajuda da Ana na mentoria do dia 1)
+
+const getProductValue = ({ price }) => parseFloat(price);
 
 const addToCart = async (event) => {
   const selectedItem = event.target.parentNode;
   const itemId = getSkuFromProductItem(selectedItem);
   const addProduct = await fetchItem(itemId);
   cartItems.appendChild(createCartItemElement(addProduct));
+  cartValues += getProductValue(addProduct);
+  addCartPrice();
   saveCartItems(cartItems.innerHTML);
 }; // Essa função coloca os itens que foram clicados no botão "Adicionar ao carrinho" no carrinho (Consegui entender o event Target graças ao Leo Araujo)
 
@@ -88,27 +93,20 @@ const clearCart = () => {
   while (cartItems.firstChild) {
     cartItems.removeChild(cartItems.firstChild);
   }
+  cartValues = 0;
+  addCartPrice();
   saveCartItems(cartItems.innerHTML);
 }; // Essa função limpa o carrinho quando executada, e limpa o storage data também.
 
-/* const cartPrice = async () => {
-  
-  const valor = 1;
-  const subtotal = document.createElement('p');
-  subtotal.innerText = `Subtotal: R$ ${valor}`;
-  cartSection.appendChild(subtotal);
-}; */
-
 window.onload = async () => { 
-  loadInteraction();
-  window.setInterval(async () => {
-    cartItems.innerHTML = getSavedCartItems(); // com a ajuda do Imar Mendes consegui retornar o valor coreto para o cartItems
-    await renderProducts(); // renderizamos os produtos na tela
-    const buttonAdd = document.querySelectorAll('.item__add'); 
-    buttonAdd.forEach((button) => { button.addEventListener('click', addToCart); }); // selecionamos o botão criado e adicionamos o item que é clicado no carrinho
-    cartItems.addEventListener('click', cartItemClickListener);// removemos o item da lista do carrinho
-  // cartPrice();
-  // cartSection.insertBefore(subtotal, cartSection.childNodes[2]);
-    removeBtn.addEventListener('click', clearCart);// removemos todos os itens do carrinho e storage data
-  }, 1000);
+  createLoadingElement();
+  cartItems.innerHTML = getSavedCartItems(); // com a ajuda do Imar Mendes consegui retornar o valor coreto para o cartItems
+  [...cartItems.children]
+    .forEach((element) => element.addEventListener('click', cartItemClickListener));
+  await renderProducts(); // renderizamos os produtos na tela
+  const buttonAdd = document.querySelectorAll('.item__add'); 
+  buttonAdd.forEach((button) => { button.addEventListener('click', addToCart); }); // selecionamos o botão criado e adicionamos o item que é clicado no carrinho
+// cartPrice();
+// cartSection.insertBefore(subtotal, cartSection.childNodes[2]);
+  removeBtn.addEventListener('click', clearCart);// removemos todos os itens do carrinho e storage data
 }; 
